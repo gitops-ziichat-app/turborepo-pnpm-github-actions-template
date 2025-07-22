261ba57287bbfb860efc3bd8f933226ab00870f2
lephuocthanh2000 <lephuocthanhnbk10h@gmail.com>
2025-07-22 11:39:28 +0700

    feat(docs): new version

--- .changeset/changesets-autogenerate.mjs
+++ .changeset/changesets-autogenerate.mjs
@@ -5,7 +5,7 @@ import fs from 'fs';
 const commitMessage = execSync('git log -1 --format=%s').toString().trim();
 
 // Define valid scopes
-const validScopes = ['docs'];
+const validScopes = ['docs','release'];
 
 // Define regex patterns
 const commitPatterns = {
